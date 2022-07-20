107276bbb24c98cb19694febd0b41a6ca9afbd9c
Piyush Kumar <108725985+piyush16git@users.noreply.github.com>
2022-07-21 01:37:52 +0530

    Add files via upload
    This was my an attempt to make a clone of official website of PARC, BITS Pilani.

---
+++ PARC Clone/task.js
@@ -0,0 +1,38 @@
+
+
+let topText = document.getElementById("top-text")
+topText.textContent = "It has come to our notice that there are some scam artists luring people into paying them to get employment at PARC. We do NOT charge an onboarding fee at PARC, nor do we have any agency authorized to recruit on our behalf. Please be vigilant."
+
+let heading = document.getElementById("heading")
+heading.textContent = "Team AtmaNirbhar is PARC's Student Division."
+
+let subHeading = document.getElementById("sub-heading")
+subHeading.textContent = "Our secret sauce, our backbone and a PARC jewel!"
+
+let p1 = document.getElementById("p1")
+p1.textContent = "Team AtmaNirbhar is the Student Division of PARC. Belonging to the Birla Institute of Technology and Science, they are passionate about making their surroundings better. They take up various community service projects to improve the lives of the under-privileged and provide them with the tools to support themselves. The students assist and complement PARC with their work in all possible aspects.   Their initiatives and boundless ideas have led to sharpening the focus of PARC."
+
+let p2 = document.getElementById("p2")
+p2.textContent = "The team originated at a meeting between two BITSian students from the batch of 2017 met with the Director of PARC, who immediately realized the potential of such a team and highlighted the positive impact it could have on PARC and its vision. Encouraged, the students reached out to the Trustees of PARC who readily agreed and put in place a Student Division for  PARC."
+
+let p21 = document.getElementById("p21")
+p21.textContent = "Several students have been looking to engage with social development and PARC gives them the opportunity and a platform to experiment and contribute.   "
+
+let p3 = document.getElementById("p3")
+p3.textContent = "Since its origin in August 2019, the student team assists PARC in conducting workshops, organizing health camps, and presenting the organization’s vision to alumni. In addition to this, the team also works to further PARC's social outreach and increase publicity. The team also undertakes projects related to college-specific problems and offers innovative solutions to institute officials for immediate implementation. The group acts as an important conduit between PARC and the BITSian Student Community.   "
+
+let p31 = document.getElementById("p31")
+p31.textContent = "The student teams' participation in PARC, while they are students, also prepares them for the next stage when they leave and become alumni, and pick up the baton as alumni to move PARC forward."
+
+let p4 = document.getElementById("p4")
+p4.textContent = "Students tend to view the issues faced by society from a different perspective and come up with creative and innovative solutions to the many problems we encounter. They regularly approach and propose new projects for PARC to initiate. The team acts as a point of contact between PARC and other student-wings of NGOs on college campuses. The team has helped in stitching collaborations with organizations such as NSS and Nirmaan, leading to a greater outreach and, subsequently, to a more significant impact on the community in Pilani. "
+
+let p41 = document.getElementById("p41")
+p41.textContent = "Most importantly, the student team is PARC's answer to continuity and sustenance. The team is filled with incoming students every year, there is no shortage of passionate and dedicated individuals ready to push the organization forward and potentially take the reins in the future. The team will ensure that PARC activities continue into the future energized with fresh, young blood every semester. "
+
+let p5 = document.getElementById("p5")
+p5.textContent = "In addition to the informal, but dedicated and structured, engagement of students with PARC by way of extra-curricular involvement, more formal engagement was put in place from the summer of 2020. PARC is now formally a Practice School station of BITS for both Practice School I and II, which are part of the academic curriculum. Students from Pilani, Hyderabad and Goa campuses have already started their formal engagement with PARC through this academic activity and are making continuous and project-specific positive contributions to the community service efforts of PARC. "
+
+let p6 = document.getElementById("p6")
+p6.textContent = "Like any other fledgling organization, the team started small, with an initial strength of just two members. However, ambition, drive, and backing from the alumni at PARC helped spur the team onto laying the groundwork to empower their college surroundings. Over the past couple of months, the team has added several members aligned with our cause- to make our surroundings a harmonious, self-sustaining society. Our purpose and ideas have since spread across states; the student team now finds itself recruiting people from BITS Hyderabad and BITS Goa as well. The team has over 25 members across several streams and colleges today. With each recruit comes an innovative solution to world problems. Each student offers a new perspective - a fresh set of young eyes into issues that PARC faces in its quest to make India more 'AtmaNirbhar.' "
+
